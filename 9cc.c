a718eecd5e90d48d3917a9db87eb9fcaa55cc5d4
paranoiavoid <k507593@kansai-u.ac.jp>
2019-08-29 12:55:30 +0000

    新しいノードを作成する関数を定義

--- 9cc.c
+++ 9cc.c
@@ -7,22 +7,39 @@
 
 //抽象構文木のノードの種類
 typedef enum {
-    ND_ADD, // +
-    ND_SUB, // -
-    ND_MUL, // *
-    ND_DIV, // /
-    ND_NUM, // 整数
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM, // 整数
 } NodeKind;
 
 typedef struct Node Node;
 
 //抽象構文木のノードの型
 struct Node {
-    NodeKind kind;  // ノードの型
-    Node *lhs;      // 左辺
-    Node *rhs;      // 右辺
-    int val;        // kindがND_NUMの場合のみ使う
+  NodeKind kind; // ノードの型
+  Node *lhs;     // 左辺
+  Node *rhs;     // 右辺
+  int val;       // kindがND_NUMの場合のみ使う
 };
+
+//新しいノードを作成する関数　二項演算子用
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+//新しいノードを作成する関数　数値用
+Node *new_node_num(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
 
 //トークンの種類
 typedef enum {
