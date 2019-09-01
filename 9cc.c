a1f72d7cd23d963f4d12e8a3ddfba2510ad87ad0
paranoiavoid <k507593@kansai-u.ac.jp>
2019-09-01 06:39:44 +0000

    再帰下降構文解析のパーサを作成

--- 9cc.c
+++ 9cc.c
@@ -24,23 +24,6 @@ struct Node {
   int val;       // kindがND_NUMの場合のみ使う
 };
 
-//新しいノードを作成する関数　二項演算子用
-Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
-  Node *node = calloc(1, sizeof(Node));
-  node->kind = kind;
-  node->lhs = lhs;
-  node->rhs = rhs;
-  return node;
-}
-
-//新しいノードを作成する関数　数値用
-Node *new_node_num(int val) {
-  Node *node = calloc(1, sizeof(Node));
-  node->kind = ND_NUM;
-  node->val = val;
-  return node;
-}
-
 //トークンの種類
 typedef enum {
   TK_RESERVED, //記号
@@ -63,6 +46,78 @@ Token *token;
 
 //入力プログラム
 char *user_input;
+
+//エラー個所を報告する
+void error_at(char *loc, char *fmt, ...);
+
+//次のトークンが期待している記号の時には、トークンを１つ読み進めて真を返す
+//それ以外の場合には偽を返す
+bool consume(char op);
+
+//次のトークンが期待している記号の時には、トークンを１つ読み進める
+//それ以外の場合にはエラーを報告する
+void expect(char op);
+
+//次のトークンが数値の場合、トークンを１つ読み進めてその数値を返す
+//それ以外の場合にはエラーを報告する
+int expect_number();
+
+bool at_eof();
+
+//新しいトークンを作成してcurに繋げる
+Token *new_token(TokenKind kind, Token *cur, char *str);
+
+//入力文字列pをトークナイズしてそれを返す
+Token *tokenize(char *p);
+
+//新しいノードを作成する関数　二項演算子用
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
+
+//新しいノードを作成する関数　数値用
+Node *new_node_num(int val);
+
+//パーサ　expr
+Node *expr();
+
+//パーサ　mul
+Node *mul();
+
+//パーサ　primary
+Node *primary();
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    error_at(token->str, "引数の個数が正しくありません");
+    return 1;
+  }
+
+  //トークナイズする
+  token = tokenize(argv[1]);
+
+  user_input = argv[1];
+
+  //アセンブリの前半部分を出力
+  printf(".intel_syntax noprefix\n");
+  printf(".global main\n");
+  printf("main:\n");
+
+  //式の最初は数でなければならないので、それをチェックして最初のmov命令を出力
+  printf("    mov rax, %d\n", expect_number());
+
+  //`+ <数>`あるいは`- <数>`というトークンの並びを消費しつつアセンブリを出力
+  while (!at_eof()) {
+    if (consume('+')) {
+      printf("    add rax, %d\n", expect_number());
+      continue;
+    }
+
+    expect('-');
+    printf("    sub rax, %d\n", expect_number());
+  }
+
+  printf("    ret\n");
+  return 0;
+}
 
 //エラー個所を報告する
 void error_at(char *loc, char *fmt, ...) {
@@ -147,36 +202,60 @@ Token *tokenize(char *p) {
   return head.next;
 }
 
-int main(int argc, char **argv) {
-  if (argc != 2) {
-    error_at(token->str, "引数の個数が正しくありません");
-    return 1;
+//新しいノードを作成する関数　二項演算子用
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+//新しいノードを作成する関数　数値用
+Node *new_node_num(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+//パーサ　expr
+Node *expr() {
+  Node *node = mul();
+
+  for (;;) {
+    if (consume('+'))
+      node = new_node(ND_ADD, node, mul());
+    else if (consume('-'))
+      node = new_node(ND_SUB, node, mul());
+    else
+      return node;
+  }
+}
+
+//パーサ　mul
+Node *mul() {
+  Node *node = primary();
+
+  for (;;) {
+    if (consume('*'))
+      node = new_node(ND_MUL, node, primary());
+    else if (consume('/'))
+      node = new_node(ND_DIV, node, primary());
+    else
+      return node;
+  }
+}
+
+//パーサ　primary
+Node *primary() {
+  // 次のトークンが"("なら、"(" expr ")"のはず
+  if (consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
   }
 
-  //トークナイズする
-  token = tokenize(argv[1]);
-
-  user_input = argv[1];
-
-  //アセンブリの前半部分を出力
-  printf(".intel_syntax noprefix\n");
-  printf(".global main\n");
-  printf("main:\n");
-
-  //式の最初は数でなければならないので、それをチェックして最初のmov命令を出力
-  printf("    mov rax, %d\n", expect_number());
-
-  //`+ <数>`あるいは`- <数>`というトークンの並びを消費しつつアセンブリを出力
-  while (!at_eof()) {
-    if (consume('+')) {
-      printf("    add rax, %d\n", expect_number());
-      continue;
-    }
-
-    expect('-');
-    printf("    sub rax, %d\n", expect_number());
-  }
-
-  printf("    ret\n");
-  return 0;
+  // そうでなければ数値のはず
+  return new_node_num(expect_number());
 }
